27f0c38273697a799d9bc1d30fe7adfb671fa326
clouless <12599965+clouless@users.noreply.github.com>
2025-04-18 13:25:53 +0200

    removed httclient - sorted imports - still not enabling

--- src/main/java/io/codeclou/ctendemo/rest/HealthEndpoint.java
+++ src/main/java/io/codeclou/ctendemo/rest/HealthEndpoint.java
@@ -9,6 +9,19 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+//import jakarta.json.stream.JsonGenerator;
+//import jakarta.json.Json;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
 @Consumes({MediaType.APPLICATION_JSON})
 @Produces({MediaType.APPLICATION_JSON})
@@ -19,7 +32,45 @@ public class HealthEndpoint {
     @Path("/test")
     @AdminOnly
     public Response getAtlassianConfluenceLog() throws Exception {
-        return Response.ok(new OkModel()).build();
+        /*StringWriter writer = new StringWriter();
+        JsonGenerator gen = Json.createGenerator(writer);
+        gen.writeStartObject()
+                .write("firstName", "Duke")
+                .write("lastName", "Java")
+                .write("age", 18)
+                .write("streetAddress", "100 Internet Dr")
+                .write("city", "JavaTown")
+                .write("state", "JA")
+                .write("postalCode", "12345")
+                .writeStartArray("phoneNumbers")
+                .writeStartObject()
+                .write("type", "mobile")
+                .write("number", "111-111-1111")
+                .writeEnd()
+                .writeStartObject()
+                .write("type", "home")
+                .write("number", "222-222-2222")
+                .writeEnd()
+                .writeEnd()
+                .writeEnd();
+        gen.close();
+        writer.toString()
+        */
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://raw.githubusercontent.com/clouless/confluence10-demo-app/refs/heads/main/LICENSE"))
+                .build();
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NEVER)
+                .connectTimeout(Duration.ofSeconds(20))
+                //.authenticator(Authenticator.getDefault())
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+
+        return Response.ok( "ok").build();
     }
 
 }
