356a823fac0cada2986ff34ce0ec75b580377279
clouless <12599965+clouless@users.noreply.github.com>
2025-04-28 08:56:07 +0200

    fix logging

--- README.md
+++ README.md
@@ -35,3 +35,21 @@ expected result
 ```
 prints license txt
 ```
+
+
+----
+
+**Health Endpoint and logging test**
+
+Performs a `log.error()` call when called.
+
+```
+curl -u admin:admin http://$CONFLUENCE:8090/rest/ctendemo/1/health/test
+```
+
+expected result: message in the confluence log
+
+```
+2025-04-28 06:55:02,267 ERROR [http-nio-8090-exec-9] [codeclou.ctendemo.rest.HealthEndpoint] getAtlassianConfluenceLog TEST_LOGGING
+ -- url: /rest/ctendemo/1/health/test | userName: admin | traceId: dc7bf1b039662ea7
+```
--- src/main/java/io/codeclou/ctendemo/rest/HealthEndpoint.java
+++ src/main/java/io/codeclou/ctendemo/rest/HealthEndpoint.java
@@ -9,16 +9,21 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Consumes({MediaType.APPLICATION_JSON})
 @Produces({MediaType.APPLICATION_JSON})
 @Path("/health/")
 public class HealthEndpoint {
 
+    private static final Logger log = LoggerFactory.getLogger(HealthEndpoint.class);
+
     @GET
     @Path("/test")
     @AdminOnly
     public Response getAtlassianConfluenceLog() throws Exception {
+        log.error("TEST_LOGGING");
         return Response.ok( "ok").build();
     }
 
